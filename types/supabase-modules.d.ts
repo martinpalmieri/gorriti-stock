b18827fbef9be361b959db1622c4684c609b7819
Martin Palmieri <martin.palmieri@bird.com>
2026-05-08 14:22:16 +0200

    Use real Supabase package types

--- types/supabase-modules.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@supabase/ssr' {
-  export function createBrowserClient(...args: unknown[]): unknown
-  export function createServerClient(...args: unknown[]): unknown
-}
-
-declare module '@supabase/supabase-js' {
-  export function createClient(...args: unknown[]): unknown
-}
